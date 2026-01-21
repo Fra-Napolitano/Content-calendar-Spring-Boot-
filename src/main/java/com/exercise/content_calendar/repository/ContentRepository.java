d63a82264db73f887fc9667e414f441f9f53d7bc
Fra-Napolitano <f.napolitano@betacom.it>
2026-01-21 11:27:57 +0100

    Repository metodi

--- src/main/java/com/exercise/content_calendar/repository/ContentRepository.java
+++ src/main/java/com/exercise/content_calendar/repository/ContentRepository.java
@@ -1,9 +1,19 @@
 package com.exercise.content_calendar.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.exercise.content_calendar.model.Content;
+import com.exercise.content_calendar.model.Status;
 
 public interface ContentRepository extends ListCrudRepository<Content, Integer>{ 
 	
+	List<Content> findAllByTitleContains(String keyword);
+	
+	@Query("""SELECT * FROM Content
+			WHERE status = :status""")
+	List<Content> listByStatus(@Param Status status);
 }
