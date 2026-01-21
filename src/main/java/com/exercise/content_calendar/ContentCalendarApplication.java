1a20d0934ee9ec1d5609a36004f1ca98e4eb98fd
Fra-Napolitano <f.napolitano@betacom.it>
2026-01-21 10:25:47 +0100

    Application

--- src/main/java/com/exercise/content_calendar/ContentCalendarApplication.java
+++ src/main/java/com/exercise/content_calendar/ContentCalendarApplication.java
@@ -1,8 +1,10 @@
 package com.exercise.content_calendar;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,6 +14,14 @@ public class ContentCalendarApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(ContentCalendarApplication.class, args);
 	RestTemplate restTemplate =  (RestTemplate) context.getBean("restTemplate");
 	System.out.println(restTemplate);
+	}
+	
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args ->
+			System.out.println("Hello Alen!");
+		
+		
 	}
 
 }
--- src/main/java/com/exercise/content_calendar/config/DataLoader.java
+++ src/main/java/com/exercise/content_calendar/config/DataLoader.java
@@ -6,7 +6,7 @@ public class DataLoader implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		System.out.println("Hello!");
 	}
 
 }
